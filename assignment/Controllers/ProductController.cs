9d3968eb6faa2476c17b0c850df2ebce402df3ff
Abdullah Rana <Abdullahalrana@gmail.com>
2021-06-15 12:22:02 +0600

    Add project files.

---
+++ assignment/Controllers/ProductController.cs
@@ -0,0 +1,77 @@
+﻿using assignment.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace assignment.Controllers
+{
+    public class ProductController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        public ProductController()
+        {
+
+        }
+
+        public ActionResult ProductManagement()
+        {
+            return View();
+        }
+        
+        public JsonResult GetProducts()
+        {
+            var productsList = db.prdc.ToList();
+            var products = productsList.Select(b => new
+            {
+                Id = b.Id,
+                ProductCode = b.ProductCode,
+                PrmoductName = b.PrmoductName,
+                Description = b.Description,
+                Price = b.Price,
+                Type = b.Type,
+                ExpiryDate = b.ExpiryDate.ToString()
+            }).ToList();
+
+            return Json(products, JsonRequestBehavior.AllowGet);
+        }
+        
+        public ActionResult SearchProduct(string searchString)
+        {
+            var productModel = db.prdc.Where(b => b.PrmoductName.ToLower().Contains(searchString.ToLower()) || b.ProductCode.ToLower().Contains(searchString.ToLower())).FirstOrDefault();
+
+            var product = new
+            {
+                Id = productModel.Id,
+                ProductCode = productModel.ProductCode,
+                PrmoductName = productModel.PrmoductName,
+                Description = productModel.Description,
+                Price = productModel.Price,
+                Type = productModel.Type,
+                ExpiryDate = productModel.ExpiryDate.ToString()
+            };
+
+            return Json(product, JsonRequestBehavior.AllowGet);
+        }
+        
+        [HttpPost]
+        public ActionResult CreateNew(Product model)
+        {
+            try
+            {
+                db.prdc.Add(model);
+                db.SaveChanges();
+                return Json(new { success = true });
+            }
+            catch
+            {
+                throw;
+            }
+            return Json(new { success = false });
+        }
+
+        
+    }
+}
---
+++ assignment/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace assignment.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        public string ProductCode { get; set; }
+        public string PrmoductName { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string Type { get; set; }
+        public DateTimeOffset ExpiryDate { get; set; }
+    }
+}
